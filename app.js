90facba4b9766de917a529a33548ad0434852f80
naitik <naitikkherala47@gmail.com>
2024-10-09 11:13:29 +0530

    first commit

--- app.js
+++ app.js
@@ -10,24 +10,36 @@ mongoose.connect('mongodb://localhost:27017/neha')
 })
 .catch((err)=>{
 console.log(err);
-
 })
-
 app.set('view engine','ejs')
 app.use(bodyparser.urlencoded({extended:true}))
-
 app.get('/',async(req,res)=>{
     const allData=await friend.find()
-    res.render('index',{allData})
+    res.render('index',{allData,update:null})
 })
-
 app.post('/createdata',async(req,res)=>{
     console.log(req.body);
     const data =req.body
-    await friend.create(data)
-
-    res.redirect('/')
-    
+    if(data.id !== "")
+    {
+       await friend.findByIdAndUpdate(data.id,data)
+    }
+    else{
+        await friend.create(data)
+    }
+    res.redirect('/') 
 })
-
+app.get('/deleteData',async(req,res)=>{
+    const deleteId = req.query.delete;
+    console.log(deleteId);
+   await friend.findByIdAndDelete(deleteId)
+    res.redirect('/');  
+})
+app.get('/updateData',async(req,res)=>{
+    const updateId = req.query.updatei;
+    console.log(updateId);
+    const update=await friend.findByIdAndUpdate(updateId)
+    const allData=await friend.find()
+    res.render('index',{allData,update})
+})
 app.listen(4000)
